399cb8cbb84e2e7a973db5c799bcda74a2fd99f4
Özalp Döndüren <ozalpd@outlook.com>
2025-10-25 21:53:18 +0300

    first commit

---
+++ MauiLocalFiles/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiLocalFiles
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiLocalFiles/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiLocalFiles")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiLocalFiles.Pages")]
